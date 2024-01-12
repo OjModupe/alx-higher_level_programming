3bbf907823109240179c8f800070b978f4da51c7
OjModupe <134464057+OjModupe@users.noreply.github.com>
2024-01-12 13:19:44 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for m in str:
         if (ord(m) >= 97 and ord(m) <= 122):
             m = chr(ord(m) - 32)
-            print("{}".format(m), end='')
-            print()
+        print("{}".format(m), end='')
+    print()
