5387eaa9d52c63be58f44212a8472eb2e80e38cd
OjModupe <134464057+OjModupe@users.noreply.github.com>
2024-03-20 00:03:03 +0000

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,6 @@
--- a script that converts hbtn_0c_0 database to UTF8
-USE 'hbtn_0c_0'
-ALTER TABLE 'first_table'
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- script that lists all records with a score >= 10 in the table second_table
+-- of the database hbtn_0c_0  in MySQL server
+-- Results display both the score and the name
+-- Records are ordered by score in descending order 
+SELECT score, name FROM second_table WHERE score >= 10
+ORDER BY score DESC;
